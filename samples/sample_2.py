ed3316e034177b76484517d27000c58d80251c88
Abi <marticiobgl@gmail.com>
2020-08-13 16:02:45 +0800

    chore: add samples on arithmetic operations

---
+++ samples/sample_2.py
@@ -0,0 +1,47 @@
+# arithmetic operations
+
+# addition
+x = 10
+y = 20
+z = x + y
+print(f"{x} + {y} = {z}")
+print(f"{x} + {y} = {x + y}")
+print(x)
+print(y)
+print(z)
+
+# subtraction
+x = 5
+y = 3
+z = x - y
+print(f"{x} - {y} = {z}")
+
+# multiplication
+x = 15
+y = 10
+z = x * y
+print(f"{x} * {y} = {z}")
+
+# division
+x = 100
+y = 2
+z = x / y
+print(f"{x} / {y} = {z}")
+
+# floor division
+x = 10
+y = 3
+z = x // y
+print(f"{x} // {y} = {z}")
+
+# modulo
+x = 10
+y = 3
+z = x % y
+print(f"{x} % {y} = {z}")
+
+# exponent **
+x = 10
+y = 2
+z = x ** y
+print(f"{x} ** {y} = {z}")
