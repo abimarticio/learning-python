9c7ce04b81d25ba0402096503cb402b9b2054d5f
Abi <marticiobgl@gmail.com>
2020-08-13 15:10:40 +0800

    chore: add exercise on i/o

---
+++ exercises/exercise_1.py
@@ -0,0 +1,26 @@
+#Ask for the following information,
+#and display the:
+#country
+#gender
+#mobile number
+#major
+#birthday
+#instagram username
+
+
+country = input("What is the name of your country? ")
+print("{}".format(country))
+gender = input("What is your gender? ")
+print("{}".format(gender))
+mobileNumber = input("What is your mobile number? ")
+print("{}".format(mobileNumber))
+print(f"You are from {country}")
+print("You are from {country}".format(country=country))
+print("You are from {location}".format(location=country))
+
+major = input("What is your major? ")
+print("{}".format(major))
+birthday = input("When is your birthday ")
+print("{}".format(birthday))
+instagram_username = input("What is your instagram username? ")
+print("{}".format(instagram_username))
