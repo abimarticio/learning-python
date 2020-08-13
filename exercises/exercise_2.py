987785aeb2308cf72e94feca250f8d1201eccff2
Abi <marticiobgl@gmail.com>
2020-08-13 16:01:24 +0800

    chore: add exercise on arithmetic operations

---
+++ exercises/exercise_2.py
@@ -0,0 +1,50 @@
+
+# addition
+print("Addition ")
+x = input("Value for x: ")
+y = input("Value for y: ")
+# x = int(input("Value for x: "))
+# y = int(input("Value for y: "))
+# z = x + y
+# x = int(x)
+# y = int(y)
+# z = x + y
+z = int(x) + int(y)
+print(f"{x} + {y} = {z}")
+
+# subtraction
+print("Subtraction ")
+x = input("Value for x: ")
+y = input("Value for y: ")
+z = int(x) - int(y)
+print(f"{x} - {y} = {z}")
+
+# multiplication
+print("Multiplication ")
+x = input("Value for x: ")
+y = input("Value for y: ")
+z = int(x) * int(y)
+print(f"{x} * {y} = {z}")
+
+# division
+print("Division ")
+x = input("Value for x: ")
+y = input("Value for y: ")
+z = int(x) / int(y)
+print(f"{x} / {y} = {z}")
+
+# modulo
+print("Modulo ")
+x = input("Value for x: ")
+y = input("Value for y: ")
+z = int(x) % int(y)
+print(f"{x} % {y} = {z}")
+
+# exponent
+
+print("Exponent ")
+x = input("Value for x: ")
+y = input("Value for y: ")
+z = int(x) ** int(y)
+print(f"{x} ** {y} = {z}")
+
