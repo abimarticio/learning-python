07439afc6a34c9c739119ffcd9f69cf2508d979e
Abi <marticiobgl@gmail.com>
2020-10-06 23:56:27 +0800

    chore: add view function

--- samples/sample_20.py
+++ samples/sample_20.py
@@ -1,3 +1,11 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    id = request.args.get("id", None)
+    if id is None:
+        return "ID not found"
+    else:
+        return " Opening database entry #{}".format(id)
